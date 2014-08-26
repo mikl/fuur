a51a79d6ea1a561dd09aead4f965f99d29206842
Mikkel Hoegh <mikkel@hoegh.org>
2014-08-27 01:08:45 +0200

    Added two-column layout for content.

---
+++ modules/fuur_base/plugins/layouts/fuur_sidebar_right/fuur-sidebar-right.tpl.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @file
+ * Site Template layout for Panels Everywhere.
+ */
+?>
+<div class="fuur-layout-sidebar-right clearfix">
+
+  <div class="layout-main">
+    <?php print render($content['main']); ?>
+  </div>
+
+  <div class="layout-sidebar">
+    <?php print render($content['sidebar']); ?>
+  </div>
+
+</div>
